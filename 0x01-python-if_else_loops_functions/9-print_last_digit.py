28dadea8d40acac217bb01ad01a14848e4c5f5eb
GorillaFu <jared@beguelin.com>
2020-12-09 03:13:15 +0000

    return last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        digit = number * -1
+        digit = digit % 10
+    else:
+        digit = number % 10
+    print('{:d}'.format(digit), end="")
+    return digit
