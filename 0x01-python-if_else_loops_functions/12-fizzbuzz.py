43ca24ab75b0cee44d0409bad1b77a75a4b0c96a
GorillaFu <jared@beguelin.com>
2020-12-09 03:19:11 +0000

    fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    a = "Fizz"
+    b = "Buzz"
+    c = "FizzBuzz"
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("{:s}".format(c), end=" ")
+            continue
+        if i % 3 == 0:
+            print("{:s}".format(a), end=" ")
+            continue
+        if i % 5 == 0:
+            print("{:s}".format(b), end=" ")
+            continue
+        print("{:d}".format(i), end=" ")
