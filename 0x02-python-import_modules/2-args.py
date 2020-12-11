4b53f13b94ea52385092ec33f8e5608c4d8fcb2e
GorillaFu <jared@beguelin.com>
2020-12-11 03:33:36 +0000

    fixed vars

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    i = 1
+    n = 1
     if len(argv) == 1:
         print("{:d} arguments.".format(len(argv) - 1))
     elif len(argv) == 2:
         print("{:d} argument:".format(len(argv) - 1))
     else:
         print("{:d} arguments:".format(len(argv) - 1))
-    while i != len(argv):
-        j = argv[i]
-        print("{:d}: {:s}".format(i, str(j)))
-        i += 1
+    while n != len(argv):
+        a = argv[n]
+        print("{:d}: {:s}".format(n, str(a)))
+        n += 1
