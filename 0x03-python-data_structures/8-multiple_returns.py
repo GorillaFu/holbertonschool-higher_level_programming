44434d0ef475b0719d086bc8b383d8abbdb0f3d7
GorillaFu <jared@beguelin.com>
2020-12-15 22:01:26 +0000

    make tuple w string char and len

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+def multiple_returns(sentence):
+    if sentence is "":
+        return 0, None
+    else:
+        i = len(sentence)
+        j = sentence[:1{
+        return i, j
