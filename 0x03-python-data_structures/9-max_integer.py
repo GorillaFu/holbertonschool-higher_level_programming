31dbea0e02c1f34435b686eece3e79b9bfd07574
GorillaFu <jared@beguelin.com>
2020-12-15 22:41:54 +0000

    used for loop instead of while

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,10 +2,8 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    i = 0
-    j = 0
-    while i < len(my_list):
-        if my_list[i] > j:
-            j = my_list[i]
-        i+=1
+    j = my_list[0]
+    for i in my_list:
+        if i > j:
+            j = i
     return j
