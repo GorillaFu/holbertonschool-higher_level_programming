755c713b1710104694bfd0accce318744805672e
GorillaFu <jared@beguelin.com>
2020-12-15 21:52:37 +0000

    fixed more whitespace

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,12 +2,11 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix is None or matrix == [[]]:
         print()
-    else:
-        for row in matrix:
-            i = 1
-            for val in row:
-                if i < len(row):
-                    print("{:d} ".format(val), end = "")
-                else:
-                    print("{:d}".format(val))
-                i+=1
+    for row in matrix:
+        i = 1
+        for val in row:
+            if i < len(row):
+                print("{:d} ".format(val), end = "")
+            else:
+                print("{:d}".format(val))
+            i+=1
