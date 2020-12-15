be970bec99f965bea53dae270020777738e65e37
GorillaFu <jared@beguelin.com>
2020-12-15 19:08:55 +0000

    added space

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     if idx < 0:
         return (None)
     if idx == 0:
-        return(my_list[0])
+        return (my_list[0])
     else:
         for i in my_list:
             if i == idx:
