9ef5750c952e75153422809353cda7f40b279338
GorillaFu <jared@beguelin.com>
2020-12-15 19:53:26 +0000

    edge case

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,6 +5,8 @@ def element_at(my_list, idx):
     if idx == 0:
         return (my_list[0])
     else:
+        if idx > len(my_list) - 1:
+            return (None)
         for i in my_list:
             if i == idx:
                 return (my_list[i])
