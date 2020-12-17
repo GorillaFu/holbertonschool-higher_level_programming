dee51084bac2a24a20d6baae0783da4994a24aa6
GorillaFu <jared@beguelin.com>
2020-12-17 19:45:07 +0000

    added whitespace around operator

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -5,5 +5,5 @@ def search_replace(my_list, search, replace):
     while i < len(my_list):
         if my_list[i] == search:
             new[i] = replace
-        i+=1
+        i += 1
     return new
