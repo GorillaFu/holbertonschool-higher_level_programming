5150bd17abd7516020ab90166c0715ad26dc9c50
GorillaFu <jared@beguelin.com>
2020-12-18 03:15:10 +0000

    shortened names

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,13 +2,13 @@
 def roman_to_int(roman_string):
     if roman_string is None or isinstance(roman_string, str) is False:
         return 0
-    conversion = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    conv = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
     i = 0
     while i < len(roman_string):
-        if i > 0 and conversion[roman_string[i]] > conversion[roman_string[i - 1]]:
-            result += conversion[roman_string[i]] - 2 * conversion[roman_string[i - 1]]
+        if i > 0 and conv[roman_string[i]] > conv[roman_string[i - 1]]:
+            result += conv[roman_string[i]] - 2 * conv[roman_string[i - 1]]
         else:
-            result += conversion[roman_string[i]]
+            result += conv[roman_string[i]]
         i += 1
     return result
