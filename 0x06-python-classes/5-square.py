d879addde3f75de46019c62131c1aa567687f260
GorillaFu <jared@beguelin.com>
2021-01-06 03:45:53 +0000

    print square method

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""define a square class"""
+
+
+class Square:
+    """square class"""
+    def __init__(self, size=0):
+        """siz attribute"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """area method return size sqrd"""
+        return(self.__size ** 2)
+
+    @property
+    def size(self):
+        """size getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size setter"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """print square method"""
+        if self.__size == 0:
+            print("")
+            return
+        for i in range(0, self.__size):
+            for j in range(0, self.__size):
+                print("#", end="")
+            print("")
