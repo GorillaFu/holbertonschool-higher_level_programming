5df2873bd75a769830ea7cb942e9e95ef629f4c4
GorillaFu <jared@beguelin.com>
2021-01-06 03:21:51 +0000

    added gets and sets

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+class Square:
+    """square class"""
+    def __init__(self, size=0):
+        """siz attribute"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """area method return size sqrd"""
+        return(self.__size ** 2)
+
+    @property
+    def size(self):
+        """size getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size setter"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
