390b15fd73530ac1f7048f727e2e8fbcbf8167ae
GorillaFu <jared@beguelin.com>
2021-01-21 02:26:59 +0000

    changed permissions

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,6 +3,7 @@
 a script that adds all arguments to a Python list, and then save them to a file
 """
 
+
 import sys
 import json
 
@@ -10,7 +11,6 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 filename = "add_item.json"
-
 with open(filename, 'a+') as f:
     pass
 
@@ -18,7 +18,6 @@ try:
     loaded = load_from_json_file(filename)
 except ValueError:
     loaded = []
-
 newitems = sys.argv[1:]
 loaded += newitems
 save_to_json_file(loaded, filename)
