8015fbb65898fad56e53ed37d76cbcc9092efd8d
GorillaFu <jared@beguelin.com>
2021-01-21 03:27:29 +0000

    student class w method that returns dict + attributes

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Student class definition"""
+
+
+class Student:
+    """
+    Studnet Class
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        student attributes
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Method that returns dict representation
+        and retrieves attributes based on attrs
+        """
+        if type(attrs) is not list:
+            return self.__dict__
+
+        attributes = {}
+
+        for i in attrs:
+            temp = getattr(self, i, None)
+            if temp is not None:
+                attributes[i] = temp
+        return attributes
