ff0fed5ad1ca21bb3e7b41b8c65c58fa413d2975
GorillaFu <jared@beguelin.com>
2021-01-27 03:15:21 +0000

    finish

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """define a base  class for making subclasses"""
+import json
+import os
 
 
 class Base:
@@ -12,3 +14,55 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is [None, ""]:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Save to file"""
+        if list_objs is None:
+            with open(cls.__name__ + ".json", "w") as file:
+                file.write("[]")
+        else:
+            l = [i.to_dictionary() for i in list_objs]
+            with open(cls.__name__ + ".json", "w") as file:
+                file.write(cls.to_json_string(l))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """onverts json to string"""
+        if json_string is None:
+            return []
+        if json_string == [] or not isinstance(json_string, str):
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Creates an object"""
+        if cls.__name__ == "Rectangle":
+            obj = cls(11, 1)
+            obj.update(**dictionary)
+            return obj
+        if cls.__name__ == "Square":
+            obj = cls(11)
+            obj.update(**dictionary)
+            return obj
+
+    @classmethod
+    def load_from_file(cls):
+        """Create"""
+        load = []
+        name = cls.__name__ + ".json"
+        if os.path.isfile(name):
+            with open(name, "r") as file:
+                str = cls.from_json_string(file.read())
+            for i in str:
+                load.append(cls.create(**i))
+            return load
+        return []
