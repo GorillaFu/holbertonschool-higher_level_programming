b8f95e1d598b8b2eed55dba6d435a5da061d5062
GorillaFu <jared@beguelin.com>
2021-01-28 22:27:15 +0000

    fixed if statement

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,7 +18,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
-        if list_dictionaries is [None, []]:
+        if list_dictionaries is in [None, []]:
             return []
         return json.dumps(list_dictionaries)
 
