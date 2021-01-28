3265789c9f62625b557a34148ded3a6b588a8558
GorillaFu <jared@beguelin.com>
2021-01-28 22:04:30 +0000

    fixed validators

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,30 +8,10 @@ class Rectangle(Base):
     rectangle subclass of base class
     """
     def __init__(self, width, height, x=0, y=0, id=None):
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be > 0")
         self.width = width
-
-        if type(heigth) is not int:
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be > 0")
         self.height = height
-
-        if type(x) is not int:
-            raise TypeError("x must be an integer")
-        if x < 0:
-            raise ValueError("x must be >= 0")
         self.x = x
-
-        if type(y) is not int:
-            raise TypeError("y must be an integer")
-        if y < 0:
-            raise ValueError("y must be >= 0")
         self.y = y
-
         super().__init__(id)
 
     @property
@@ -59,7 +39,7 @@ class Rectangle(Base):
         """validate value and width to value"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
@@ -68,7 +48,7 @@ class Rectangle(Base):
         """validate value and set height to value"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
