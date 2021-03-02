5b9a67dc8d105742111ff1659b6ca285c28d817b
GorillaFu <jared@beguelin.com>
2021-03-02 19:05:01 +0000

    fixed table name

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 -- create table with id that defaults to 1 and checks if unique
-CREATE TABLE IF NOT EXISTS id_not_null (
+CREATE TABLE IF NOT EXISTS unique_id (
 id INT default 1 UNIQUE,
 name VARCHAR(256)
 );
