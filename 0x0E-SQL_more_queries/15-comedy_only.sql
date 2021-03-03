a7f348d616284291bb55b43827fe02cd0a27b2ed
GorillaFu <jared@beguelin.com>
2021-03-03 03:44:41 +0000

    print all comedy shows

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,2 @@
+-- list all comedy shows in database
+SELECT tv_shows.title FROM tv_genres INNER JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id JOIN tv_shows ON tv_show_genres.show_id=tv_shows.id WHERE tv_genres.name='Comedy' ORDER BY tv_shows.title ASC;
