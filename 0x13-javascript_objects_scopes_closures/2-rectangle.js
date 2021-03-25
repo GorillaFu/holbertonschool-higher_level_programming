12b31f437b103b2daec3e383108df4735a55c4a8
GorillaFu <jared@beguelin.com>
2021-03-25 02:35:40 +0000

    fixed comments

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 // rectangle class w constructor
 // includes width and height params
+// if no arguments or neg args dont define obj
 module.exports = class Rectangle {
-    constructor (width, height) {
-	if (width !== undefined && width > 0 && height !== undefined && height > 0) {
-	    this.width = width;
-	    this.height = height;
+    constructor (w, h) {
+	if (w !== undefined && w > 0 && h !== undefined && h > 0) {
+	    this.width = w;
+	    this.height = h;
 	}
     }
 };
