ba62f068ba6646667c317cf27d818520b9a0acf2
GorillaFu <jared@beguelin.com>
2021-03-25 02:27:00 +0000

    rectangle class w params and constructor

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// rectangle class w constructor
+// includes width and height params
+module.exports = class Rectangle {
+    constructor (width, height) {
+	this.width = width;
+	this.height = height;
+    }
+};
