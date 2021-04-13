520d8f7f7c0021d5a011dd682175e7f6e33d9457
GorillaFu <jared@beguelin.com>
2021-04-13 20:03:09 +0000

    README file + script that fetches content of url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# python script that fetches url content and displays in specified format
+import urllib.request
+
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+   html = response.read()
+   print("Body response:")
+   print("\t- type: {}".format(type(html)))
+   print("\t- content: {}".format(html))
+   print("\t- utf8 content: {}".format(html.decode("utf-8")))
