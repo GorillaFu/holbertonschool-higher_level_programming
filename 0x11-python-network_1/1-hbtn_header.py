3892d4fc267a4a7ea38683d2aa97755c3716dab6
GorillaFu <jared@beguelin.com>
2021-04-13 21:20:40 +0000

    checker test

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-# Python script that takes in a URL, displays val of X-Request-Id
+# python script that takes in a URL, displays val of X-Request-Id
 import urllib.request
 import sys
 
-# script
 with urllib.request.urlopen(sys.argv[1]) as response:
     html = response.headers['X-Request-Id']
     print(html)
