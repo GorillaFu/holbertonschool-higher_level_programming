3024740a6b6067abc8439e42a80d8f5f837dea1f
GorillaFu <jared@beguelin.com>
2021-05-05 02:37:02 +0000

    name edits

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 
-const URL = process.argv[2]
+const URL = process.argv[2];
 const request = require('request');
-request(URL, function (error, reponse, body) {
-  if (!error) {
+request(URL, function (err, response, body) {
+  if (!err && response.statusCode === 200) {
     const results = JSON.parse(body).results;
     let count = 0;
-    for (const i of results) {
-      const characters = results[i].characters;
-      for (const j in characters) {
-        if (characters[j].search('/18/') !== 0) {
+    for (const x in results) {
+      const characters = results[x].characters;
+      for (const y in characters) {
+        if (characters[y].search('/18/') > 0) {
           count++;
         }
       }
