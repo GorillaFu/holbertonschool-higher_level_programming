a7b955f56b7d97b6f165e28fe6056d4e01b15528
GorillaFu <jared@beguelin.com>
2021-05-05 02:41:18 +0000

    request contents of wp, store in file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+const URL = process.argv[2];
+const file = process.argv[3];
+
+request(URL, function (err, response, body) {
+  if (!err && response.statusCode === 200) {
+    fs.writeFile(file, body, 'utf-8', function (err, data) {
+      if (err) {
+        return console.log(err);
+      }
+    });
+  }
+});
